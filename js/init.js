619c08a68c830e2c38669c8e1664d266fd66a61c
chatIC41M <81340046+chatIC41M@users.noreply.github.com>
2021-03-24 20:26:02 -0600

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+    apiKey: "AIzaSyA8AuuUmJtmKD6hDoL7Fz5hVMnd31ZiqVY",
+    authDomain: "chat41gb.firebaseapp.com",
+    projectId: "chat41gb",
+    storageBucket: "chat41gb.appspot.com",
+    messagingSenderId: "723171617874",
+    appId: "1:723171617874:web:6d50e8e3ef746c467a838b",
+    measurementId: "G-H4TN41FE3D"
+    });
