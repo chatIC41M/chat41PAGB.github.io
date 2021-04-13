76ff6c56b6455eee203c3ad2e5dcc83d7132a268
chatIC41M <81340046+chatIC41M@users.noreply.github.com>
2021-04-13 14:57:37 -0500

    Add files via upload

---
+++ js/init.js
@@ -0,0 +1,18 @@
+/*  Inicializa Firebase con la
+ * configuración del proyecto.
+ * Revisa la configuración en tu
+ * servidor de Firebase y cópiala
+ * aquí sustituyendo los
+ * asteriscos. Los campos deben
+ * quedar igual que en tu
+ * servidor. */
+// @ts-ignore
+firebase.initializeApp({
+  apiKey: "AIzaSyCGXQ5fa8yNGuChekkIEjIL3XYkFkxaNSI",
+  authDomain: "bggradm-ae902.firebaseapp.com",
+  projectId: "bggradm-ae902",
+  storageBucket: "bggradm-ae902.appspot.com",
+  messagingSenderId: "842161842084",
+  appId: "1:842161842084:web:317b386363c50217a2c331",
+  measurementId: "G-5CPZV8N0L2"
+});
